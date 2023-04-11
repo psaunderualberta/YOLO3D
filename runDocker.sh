74926420641caebb4d020644ec4be389290bfb19
psaunderualberta <psaunder@ualberta.ca>
2023-04-11 13:14:21 -0600

    Commented out inference command

--- runDocker.sh
+++ runDocker.sh
@@ -1,8 +1,10 @@
 docker run -it --rm --gpus all -v ${PWD}:/yolo3d -it ruhyadi/yolo3d:latest
-python inference.py \
-    --weights yolov5s.pt \
-    --source data/KITTI/data_object_image_2/training \
-    --reg_weights weights/resnet18.pkl \
-    --model_select resnet18 \
-    --output_path runs/ \
-    --save_result
+
+# Inference script
+# python inference.py \
+#     --weights yolov5s.pt \
+#     --source data/KITTI/data_object_image_2/training \
+#     --reg_weights weights/resnet18.pkl \
+#     --model_select resnet18 \
+#     --output_path runs/ \
+#     --save_result
